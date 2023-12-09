55c0e57aea7704122357119b6b608f5b6814b5fa
Vitalii Goncharov <1c.audit@gmail.com>
2023-12-09 12:35:30 +0200

    refactor nvim install script

--- src/nvim/install.sh
+++ src/nvim/install.sh
@@ -1,10 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
-# ~Manually install NeoVim
-cd ~/.local
+create_folder_if_not_exists() {
+	local folder_path="$1"
+
+	if [ ! -d "$folder_path" ]; then
+		mkdir -p "$folder_path"
+		echo "Folder created: $folder_path"
+	else
+		echo "Folder already exists: $folder_path"
+	fi
+}
+
+create_folder_if_not_exists "$HOME/.local"
+cd "$HOME/.local"
+
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract
-mkdir ~/.local/bin
+
+create_folder_if_not_exists ~/.local/bin
 ln -s ~/.local/squashfs-root/AppRun ~/.local/bin/nvim
