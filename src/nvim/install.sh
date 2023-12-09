befb87bb9cad965a8de4caf46210bf5dbb5e4778
Vitalii Goncharov <1c.audit@gmail.com>
2023-12-09 23:07:31 +0200

    add lazygit ripgrep and fzf

--- src/nvim/install.sh
+++ src/nvim/install.sh
@@ -12,11 +12,17 @@ create_folder_if_not_exists() {
 	fi
 }
 
+echo "Installing neovim"
 create_folder_if_not_exists "/usr/local/lib/nvim"
 cd "/usr/local/lib/nvim"
-
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract
-
 ln -s /usr/local/lib/nvim/squashfs-root/AppRun /usr/local/bin
+
+echo "Installing lazygit"
+wget https://github.com/jesseduffield/lazygit/releases/download/v0.40.2/lazygit_0.40.2_Linux_x86_64.tar.gz
+tar -xvzf lazygit_0.40.2_Linux_x86_64.tar.gz lazygit -C /usr/local/bin
+
+echo "Installing fzf and ripgrep"
+apt-get install -y fzf ripgrep
