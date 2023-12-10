fd9e528fc84fae84f9be5d80d0ab7f8574f8cb06
Vitalii Goncharov <1c.audit@gmail.com>
2023-12-10 20:05:16 +0200

    add fd

--- src/nvim/install.sh
+++ src/nvim/install.sh
@@ -23,17 +23,32 @@ ln -s /usr/local/lib/nvim/squashfs-root/AppRun /usr/local/bin/nvim
 
 echo "Installing lazygit"
 echo "=============================================="
-# example tag_name: v0.28.2
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+# example
+# tag_name: v0.40.2
+# file link: https://github.com/jesseduffield/lazygit/releases/download/v0.40.2/lazygit_0.40.2_Linux_x86_64.tar.gz
+VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
 
 echo "Installing ripgrep"
 echo "=============================================="
-# example tag_name: 0.28.2
-RIPGREP_VERSION=$(curl -s "https://api.github.com/repos/BurntSushi/ripgrep/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
-curl -LO "https://github.com/BurntSushi/ripgrep/releases/latest/download/ripgrep-${RIPGREP_VERSION}-x86_64-unknown-linux-musl.tar.gz"
-tar xf ripgrep-"${RIPGREP_VERSION}"-*.tar.gz
-cd ripgrep-"${RIPGREP_VERSION}"-x86_64-unknown-linux-musl
+# example
+# tag_name: 14.0.3
+# file link: https://github.com/BurntSushi/ripgrep/releases/download/14.0.3/ripgrep-14.0.3-x86_64-unknown-linux-musl.tar.gz
+VERSION=$(curl -s "https://api.github.com/repos/BurntSushi/ripgrep/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
+curl -LO "https://github.com/BurntSushi/ripgrep/releases/latest/download/ripgrep-${VERSION}-x86_64-unknown-linux-musl.tar.gz"
+tar xf ripgrep-"${VERSION}"-*.tar.gz
+cd ripgrep-"${VERSION}"-x86_64-unknown-linux-musl
 sudo install rg /usr/local/bin
+
+echo "Installing fd"
+echo "=============================================="
+# example
+# tag_name: v8.7.1
+# file link: https://github.com/sharkdp/fd/releases/download/v8.7.1/fd-v8.7.1-x86_64-unknown-linux-musl.tar.gz
+VERSION=$(curl -s "https://api.github.com/repos/sharkdp/fd/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
+curl -LO "https://github.com/sharkdp/fd/releases/latest/download/fd-${VERSION}-x86_64-unknown-linux-musl.tar.gz"
+tar xf fd-"${VERSION}"-*.tar.gz
+cd fd-"${VERSION}"-x86_64-unknown-linux-musl
+sudo install fd /usr/local/bin
