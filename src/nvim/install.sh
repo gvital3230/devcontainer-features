f3126d4e9976d7f8b6009be83582dc943c2ac41a
Vitalii Goncharov <1c.audit@gmail.com>
2023-12-10 20:27:04 +0200

    detect version with jq instead of curl

--- src/nvim/install.sh
+++ src/nvim/install.sh
@@ -26,7 +26,7 @@ echo "=============================================="
 # example
 # tag_name: v0.40.2
 # file link: https://github.com/jesseduffield/lazygit/releases/download/v0.40.2/lazygit_0.40.2_Linux_x86_64.tar.gz
-VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | jq -r '.tag_name | ltrimstr("v")')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
@@ -36,7 +36,7 @@ echo "=============================================="
 # example
 # tag_name: 14.0.3
 # file link: https://github.com/BurntSushi/ripgrep/releases/download/14.0.3/ripgrep-14.0.3-x86_64-unknown-linux-musl.tar.gz
-VERSION=$(curl -s "https://api.github.com/repos/BurntSushi/ripgrep/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
+VERSION=$(curl -s "https://api.github.com/repos/BurntSushi/ripgrep/releases/latest" | jq -r '.tag_name')
 curl -LO "https://github.com/BurntSushi/ripgrep/releases/latest/download/ripgrep-${VERSION}-x86_64-unknown-linux-musl.tar.gz"
 tar xf ripgrep-"${VERSION}"-*.tar.gz
 cd ripgrep-"${VERSION}"-x86_64-unknown-linux-musl
@@ -47,7 +47,7 @@ echo "=============================================="
 # example
 # tag_name: v8.7.1
 # file link: https://github.com/sharkdp/fd/releases/download/v8.7.1/fd-v8.7.1-x86_64-unknown-linux-musl.tar.gz
-VERSION=$(curl -s "https://api.github.com/repos/sharkdp/fd/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
+VERSION=$(curl -s "https://api.github.com/repos/sharkdp/fd/releases/latest" | jq -r '.tag_name')
 curl -LO "https://github.com/sharkdp/fd/releases/latest/download/fd-${VERSION}-x86_64-unknown-linux-musl.tar.gz"
 tar xf fd-"${VERSION}"-*.tar.gz
 cd fd-"${VERSION}"-x86_64-unknown-linux-musl
